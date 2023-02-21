6d67e438afebdbf3e8440a38ac19887e065d4947
GiangPT <giangbeo159@gmail.com>
2023-02-21 17:35:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginScreen />
+    </div>
+  );
+}
+
+export default App;
